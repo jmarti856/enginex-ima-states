e9dec7d243c1bf0d36ec86bc0b7b826361bc6449
jmarti856 <jmarti856@gmail.com>
2021-02-07 14:05:14 +0100

    Start

---
+++ codec.h
@@ -0,0 +1,17 @@
+
+#ifndef codec_h
+#define codec_h
+
+#include "Types.h"
+
+struct CodecState
+{
+	int valprev;
+	int index;
+};
+
+void encode(CodecState* state, s16* input, int numSamples, u8* output);
+void decode(CodecState* state, u8* input, int numSamples, s16* output);
+
+
+#endif
